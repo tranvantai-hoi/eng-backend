8d6fbb2445c597c0b6f3da11e0ce7799cf37133f
tranvantai-hoi <tranvantai@ttn.edu.vn>
2025-12-01 17:01:44 +0700

    Update registrationRoutes.js

--- routes/registrationRoutes.js
+++ routes/registrationRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const registrationController = require('../controllers/registrationController');
 
-// Route gửi OTP: POST /api/registrations/send-otp
+// POST /api/registrations/send-otp
 router.post('/send-otp', registrationController.sendOtp);
 
-// Route đăng ký: POST /api/registrations
+// POST /api/registrations (Xác thực & Đăng ký)
 router.post('/', registrationController.register);
 
-// Route lịch sử
+// GET /api/registrations/history/:mssv
 router.get('/history/:mssv', registrationController.getHistory);
 
 module.exports = router;
