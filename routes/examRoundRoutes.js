1a6b5914bada6e1673484f944a10e4389fccfd4d
tranvantai-hoi <tranvantai@ttn.edu.vn>
2025-12-02 09:52:44 +0700

    Add active route handler to examRoundRoutes

--- routes/examRoundRoutes.js
+++ routes/examRoundRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const {
   getRounds,
+  getRounds,
   createRound,
   updateRound,
   deleteRound
 } = require('../controllers/examRoundController');
-
+router.get('/', active);
 router.get('/', getRounds);
 router.post('/', createRound);
 router.put('/:id', updateRound);
