a46f93b4379a5d0728fa8fed8260282b45ec61f0
tranvantai-hoi <tranvantai@ttn.edu.vn>
2025-12-04 14:36:55 +0700

    Create mailer.js

---
+++ models/mailer.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+// Cấu hình Transporter (người vận chuyển)
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+// Hàm gửi mail
+const sendEmail = async (to, subject, htmlContent) => {
+  try {
+    const mailOptions = {
+      from: `"Hệ Thống Đăng Ký Thi" <${process.env.EMAIL_USER}>`,
+      to: to,
+      subject: subject,
+      html: htmlContent, // Nội dung HTML đẹp hơn text thường
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent: ' + info.response);
+    return info;
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw error;
+  }
+};
+
+module.exports = { sendEmail };
