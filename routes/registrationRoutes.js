7958774744907600d32407b1a2b0b0da4ebeda5c
tranvantai-hoi <tranvantai@ttn.edu.vn>
2025-12-04 16:14:35 +0700

    Update registrationRoutes.js

--- routes/registrationRoutes.js
+++ routes/registrationRoutes.js
@@ -12,7 +12,7 @@ const {
 // 1. Gửi mã OTP
 // Frontend gọi: /registrations/send-otp
 // Định nghĩa: /registrations/send-otp (Khớp 100%)
-router.post('/registrations/send-otp', sendOtp);
+router.post('/send-otp', sendOtp);
 
 // 2. Tạo đăng ký thi
 // Frontend gọi: /register
