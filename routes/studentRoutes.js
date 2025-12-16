6e71ece2efb9bd8b8fa028f302a1960f9e18211d
tranvantai-hoi <tranvantai@ttn.edu.vn>
2025-12-16 20:31:31 +0700

    Update studentRoutes.js

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getStudents, updateContactInfo, importStudents } = require('../controllers/studentController');
+const { getStudents, updateContactInfo, updateStudentFullInfo, importStudents } = require('../controllers/studentController');
 
 router.get('/', getStudents);
 router.post('/update-contact', updateContactInfo);
+router.post('/update', updateStudentFullInfo);
 router.post('/import', importStudents);
 module.exports = router;
 
